733c04366abdcf29c288faba9093aa4ff65371cd
sachinpatel16 <sachinpatel7496007@gmail.com>
2025-07-23 22:58:33 +0530

    add dev

--- config/settings/development.py
+++ config/settings/development.py
@@ -20,7 +20,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'bartr',
         'USER': 'postgres',
-        'PASSWORD': 'dwij9143',
+        'PASSWORD': 'admin',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
